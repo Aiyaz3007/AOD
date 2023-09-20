5b9a1bcadf161644c798ddc92726fe3bd3baf510
Aiyaz3007 <maiyaz3007@gmail.com>
2023-09-21 01:07:40 +0530

    update in graph

--- utils.py
+++ utils.py
@@ -2,9 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def updateTrainLoss(epochs:list,loss:list):
+    plt.title("train loss")
     plt.plot(epochs,loss)
     plt.savefig("TrainLoss.png")
 
 def updateValLoss(epochs:list,loss:list):
+    plt.title("val loss")
     plt.plot(epochs,loss)
     plt.savefig("ValLoss.png")
