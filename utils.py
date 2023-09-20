33638bb588002704a1af95c2d87a9ce833ce5e79
Aiyaz3007 <maiyaz3007@gmail.com>
2023-09-21 02:00:13 +0530

    fix error in graph

--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-figure, axis = plt.subplots(1, 2)
+figure, axis = plt.subplots(2, 1)
 
 def updateLoss(epochs:list,trainloss:list,valloss:list):
-    plt.title("train loss")
-    axis[0,0].plot(epochs,trainloss)
-    axis[0,1].plot(epochs,valloss)
+    axis[0].plot(epochs,trainloss)
+    axis[0].set_title("Train Loss")
+    axis[1].plot(epochs,valloss)
+    axis[1].set_title("Val Loss")
+
     figure.savefig("Loss.png")
